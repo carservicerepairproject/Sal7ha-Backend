dfed37df0a2b9abcbeab4b760ec2fbd99539f7a3
marwan mohamed <marwan.mohamed7287@gmail.com>
2025-12-12 15:18:54 +0200

    updated config/scripts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma', // adjust if your schema path differs
+  schema: 'prisma/schema.prisma',
   datasource: {
     url: env('DATABASE_URL'),
   },
 });
-  
