74ff805002d6a0b62c007e2f616601d2c4466707
marwan mohamed <marwan.mohamed7287@gmail.com>
2025-12-12 10:10:30 +0200

    Fixed JWT Secret Config

--- src/client/auth/client.auth.module.ts
+++ src/client/auth/client.auth.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { ClientAuthController } from './client.auth.controller';
 import { ClientAuthService } from './client.auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { JwtStrategy } from '../strategy';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [ClientAuthController],
   providers: [ClientAuthService, JwtStrategy],
 })
--- src/shop/auth/business.auth.module.ts
+++ src/shop/auth/business.auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { BusinessAuthController } from './business.auth.controller';
 import { BusinessAuthService } from './business.auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { BusinessJwtStrategy } from '../strategy/business-jwt.strategy';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [BusinessAuthController],
-  providers: [BusinessAuthService],
+  providers: [BusinessAuthService, BusinessJwtStrategy],
 })
 export class BusinessAuthModule {}
